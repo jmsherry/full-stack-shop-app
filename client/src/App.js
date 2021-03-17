ee69cea03df4ba64dd48a13d207d4f85f2cdb809
James Sherry <james.m.sherry@googlemail.com>
2021-03-17 19:39:04 +0000

    client initial files and basic connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      console.log("getting");
+      try {
+        const response = await fetch("/api/v1/products");
+        console.log(
+          "🚀 ~ file: App.js ~ line 12 ~ getData ~ response",
+          response
+        );
+
+        if (!response.ok) throw response;
+
+        const data = await response.json();
+        console.log("🚀 ~ file: App.js ~ line 15 ~ getData ~ data", data);
+        setProducts(data);
+      } catch (err) {
+        console.log("error", err.message || err.statusText);
+      }
+    };
+    getData();
+  }, [setProducts]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Products</h1>
+      <ul>
+        {products.map(({ title, price }) => (
+          <li key={title}>
+            {title} (£{price.toFixed(2)})
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
