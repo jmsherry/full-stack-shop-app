26b7a340a3bc3f9e681cf1048e56974265f29909
James Sherry <james.m.sherry@googlemail.com>
2021-03-31 15:02:52 +0100

    sorted testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("The App Component", () => {
+  test("it renders", async () => {
+    render(<App />);
+    const headerElement = await screen.findByText(/Todos Full-Stack App/i);
+    expect(headerElement).toBeInTheDocument();
+  });
 });
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+Object.defineProperty(global.self, 'crypto', {
+  value: {
+    getRandomValues: arr => crypto.randomBytes(arr.length)
+  }
+})
+global.crypto.subtle = {} 
